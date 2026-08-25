062f44303b24f6d2c0816808c85a4166100d17ca
pviprey <viprey.pierre@gmail.com>
2026-08-25 18:06:43 +0200

    feat(aside): Providing (nearly) every icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,16 +13,46 @@ import { Contacts } from './contacts/contacts';
 export class AppComponent {
   title = 'angular.pierreviprey.fr';
 
-  backend: { [key: string]: { path: string, grade: number } } = {};
-  frontend: { [key: string]: { path: string, grade: number } } = {};
-  database: { [key: string]: { path: string, grade: number } } = {};
-  softSkills: { [key: string]: { path: string, grade: number } } = {};
-  testing: { [key: string]: { path: string, grade: number } } = {};
-  operatingSystem: { [key: string]: { path: string, grade: number } } = {};
+  backend: { [key: string]: { path: string, grade: number } } = {
+    'java': { path: '/java.svg', grade: 8 },
+    'springBoot': { path: '/springboot.svg', grade: 6 },
+  };
+  frontend: { [key: string]: { path: string, grade: number } } = {
+    'html': { path: '/html5.svg', grade: 8 },
+    'sass': { path: '/sass.svg', grade: 7 },
+    'typescript': { path: '/typescript.svg', grade: 7 },
+    'angular': { path: '/angular.svg', grade: 7 },
+    'ngrx': { path: '/ngrx.svg', grade: 4 },
+    'react': { path: '/react.svg', grade: 2 },
+  };
+  database: { [key: string]: { path: string, grade: number } } = {
+    'SQL': { path: '/sql.svg', grade: 8 },
+  };
+  softSkills: { [key: string]: { path: string, grade: number } } = {
+    'Teamwork': { path: '/teamwork.svg', grade: 9 },
+    'Autonomy': { path: '/autonomy.svg', grade: 10 },
+    'Communication': { path: '/communication.svg', grade: 8 },
+    'Adaptability': { path: '/adaptability.svg', grade: 8 },
+  };
+  practice: { [key: string]: { path: string, grade: number } } = {
+    'TDD': { path: '/tdd.svg', grade: 6 },
+    'Agile': { path: '/agile.svg', grade: 8 },
+    'Unit Testing': { path: '/unitTesting.svg', grade: 9 },
+  };
+  operatingSystem: { [key: string]: { path: string, grade: number } } = {
+    'linux': { path: '/linux.svg', grade: 8 },
+    'Windows': { path: '/windows.svg', grade: 8 },
+  };
   language: { [key: string]: { path: string, grade: number } } = {
     'fr': { path: '/Flag_of_France.svg', grade: 10 },
     'en': { path: '/Flag_of_the_United_Kingdom.svg', grade: 9 },
     'de': { path: '/Flag_of_Germany.svg', grade: 2 },
+  };
+  devOps: { [key: string]: { path: string, grade: number } } = {
+    'git': { path: '/git.svg', grade: 8 },
+    'docker': { path: '/docker.svg', grade: 6 },
+    'jenkins': { path: '/jenkins.svg', grade: 6 },
+    'sonarqube': { path: '/sonarqube.svg', grade: 6 },
   };
   
 
